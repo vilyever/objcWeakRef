034db26b806416c05d9ce73e8849817c6163928e
vilyever <250450035@163.com>
2016-08-03 17:47:49 +0800

    Initial Commit

---
+++ objcWeakRef/objcWeakRef.h
@@ -0,0 +1,19 @@
+//
+//  objcWeakRef.h
+//  objcWeakRef
+//
+//  Created by FTET on 16/8/3.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcWeakRef.
+FOUNDATION_EXPORT double objcWeakRefVersionNumber;
+
+//! Project version string for objcWeakRef.
+FOUNDATION_EXPORT const unsigned char objcWeakRefVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcWeakRef/PublicHeader.h>
+
+
